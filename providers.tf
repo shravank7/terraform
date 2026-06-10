5a7bd7f7dcf400b49ee0dfbd288d144f9e2775d7
shravank7 <104685502+shravank7@users.noreply.github.com>
2026-06-10 16:21:32 +0530

    Create providers.tf

---
+++ providers.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    local = {
+      source = "hashicorp/local"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+}
+
+provider "local" {}
+provider "random" {}
